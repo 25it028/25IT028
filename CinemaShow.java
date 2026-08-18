0a8fc60929b4fc9b877266f21acb403726300c17
TANVI <25it028@chearusat.edu.in>
2026-08-18 08:29:37 +0530

    first commit

---
+++ CinemaShow.java
@@ -0,0 +1,71 @@
+public class CinemaShow {
+
+    private String title;
+    private int seatsAvailable;
+    private final int capacity;
+    private static int totalBooked = 0;
+
+    // Constructor
+    public CinemaShow(String title, int capacity) {
+        this.title = title;
+        this.capacity = capacity;
+        this.seatsAvailable = capacity;
+    }
+
+    // Constructor with default capacity = 100
+    public CinemaShow(String title) {
+        this(title, 100);
+    }
+
+    // Book seats
+    public boolean book(int n) {
+        if (n > 0 && n <= seatsAvailable) {
+            seatsAvailable -= n;
+            totalBooked += n;
+            return true;
+        }
+        return false;
+    }
+
+    // Cancel seats
+    public void cancel(int n) {
+        if (n > 0 && n <= capacity - seatsAvailable) {
+            seatsAvailable += n;
+            totalBooked -= n;
+        }
+    }
+
+    // Getter
+    public int getSeatsAvailable() {
+        return seatsAvailable;
+    }
+
+    // Static getter
+    public static int getTotalBooked() {
+        return totalBooked;
+    }
+
+    public static void main(String[] args) {
+
+        CinemaShow show = new CinemaShow("Avengers", 50);
+
+        System.out.println("Book 20 seats: " + show.book(20));
+        System.out.println("Seats Available: " + show.getSeatsAvailable());
+
+        System.out.println("Book 25 seats: " + show.book(25));
+        System.out.println("Seats Available: " + show.getSeatsAvailable());
+
+        System.out.println("Book 10 seats: " + show.book(10));
+        System.out.println("Seats Available: " + show.getSeatsAvailable());
+
+        show.cancel(15);
+
+        System.out.println("After cancelling 15 seats:");
+        System.out.println("Seats Available: " + show.getSeatsAvailable());
+
+        System.out.println("Book 10 seats: " + show.book(10));
+        System.out.println("Seats Available: " + show.getSeatsAvailable());
+
+        System.out.println("Total Booked Seats: " + CinemaShow.getTotalBooked());
+    }
+}
---
+++ Driver1.java
@@ -0,0 +1,24 @@
+public class Driver1 {
+    public static void main(String[] args) {
+
+        PasswordChecker pc = new PasswordChecker();
+
+        String[] passwords = {
+            "abc",
+            "Abcd1234!",
+            "Password",
+            "pass123",
+            "Hello@123"
+        };
+
+        for (String pw : passwords) {
+            System.out.println("Password: " + pw);
+            System.out.println("Length >= 8: " + pc.hasLength(pw));
+            System.out.println("Contains Uppercase: " + pc.hasUpperCase(pw));
+            System.out.println("Contains Digit: " + pc.hasDigit(pw));
+            System.out.println("Contains Special Character: " + pc.hasSpecialChar(pw));
+            System.out.println("Strength: " + pc.strength(pw));
+            System.out.println();
+        }
+    }
+}
